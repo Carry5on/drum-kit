7d0ee5acc34a74d70e55e26294b353aa30f80628
Carry5on <kinga.top.8@gmail.com>
2020-08-11 22:03:36 +0200

    add js file

--- main.js
+++ main.js
@@ -13,15 +13,4 @@ document.addEventListener('keyup', function(e) {
     key.classList.remove('transfered');
 });
 
-// document.addEventListener('mousedown', function(e) {
-//     const audio = drum(`audio[data-key="${e.keyCode}"]`);
-//     const key = drum(`.drum[data-key="${e.keyCode}"]`);
-//     key.classList.add('transfered');
-//     if(!audio) return;
-//     audio.play();
-// });
-// document.addEventListener('mouseup', function(e) {
-//     const key = document.querySelector(`.drum[data-key="${e.keyCode}"]`);
-//     key.classList.remove('transfered');
-// });
 
